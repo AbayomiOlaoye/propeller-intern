6a9035f29640addaf21444d9948d774c64a88503
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-04-10 18:20:12 +0100

    Update app.js content  - link properties

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's do this 💪
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Curious about Using Drones on your Worksite?
         </a>
       </header>
     </div>
